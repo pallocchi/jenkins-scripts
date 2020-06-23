67346fa39bff1db530382c9af144c80973f8cfd0
Pablo Pallocchi <pablopallocchi@gmail.com>
2020-06-23 15:20:42 -0300

    testing tag with latest

--- vars/test.groovy
+++ vars/test.groovy
@@ -4,6 +4,7 @@ def call(Boolean skip = false) {
   echo "Skip tests? $skip"
   if (!skip) {
     echo "Executing JUnit tests"
+    echo "Trying tags with latest"
     sh './gradlew clean test --no-daemon'
   } else {
     echo "Skipping JUnit tests"
